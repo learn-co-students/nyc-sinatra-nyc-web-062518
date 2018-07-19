b21d73a18f950d3ce4eeae5327713f8152a44f05
Daniel Ran <danielr1735@gmail.com>
2018-07-19 16:11:02 -0400

    added functionality

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,7 +1,7 @@
 class FiguresController < ApplicationController
 
   get '/figures' do
-    @figures = Figures.all
+    @figures = Figure.all
     erb :'figures/index'
   end
 
@@ -18,8 +18,27 @@ class FiguresController < ApplicationController
     erb :'figures/edit'
   end
 
-  post '/figures/:id' do
-    binding.pry
+  get '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    erb :'figures/show'
+  end
+
+  post '/figures' do
+    @figure = Figure.create(params[:figure])
+    @figure.title_ids = params[:title][:id]
+    if !params[:title][:name].empty?
+       new_title = Title.create(name: params[:title][:name])
+       @figure.titles << new_title
+    end
+
+    @figure.landmark_ids = params[:landmark][:id]
+    if !params[:landmark][:name].empty?
+      new_landmark = Landmark.create(name: params[:landmark][:name], year_completed: params[:landmark][:year_completed])
+      @figure.landmarks << new_landmark
+    end
+
+    @figure.save
+    redirect to '/figures'
   end
 
 end
