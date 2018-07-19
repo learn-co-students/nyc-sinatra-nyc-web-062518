6b41c964c00189927b542776e9b89191e4fb5760
Daniel Ran <danielr1735@gmail.com>
2018-07-19 11:43:18 -0400

    LandmarksController.rb - create ability to add landmark and associate a figure with it

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,31 @@
 class LandmarksController < ApplicationController
-  
+
+  get '/landmarks' do
+    @landmarks = Landmark.all
+    erb :'landmarks/index'
+  end
+
+  get '/landmarks/new' do
+    @figures = Figure.all
+    erb :'landmarks/new'
+  end
+
+  get '/landmarks/:id' do
+    @landmark = Landmark.find(params[:id])
+    erb :'landmarks/show'
+  end
+
+  post '/landmarks' do
+    # binding.pry
+    landmark = Landmark.create(params[:landmark])
+    if !params[:figure][:name].empty?
+      figure = Figure.create(name: params[:figure][:name])
+    else
+      figure = Figure.find(params[:figure][:id])
+    end
+    landmark.figure = figure
+    landmark.save
+    redirect to '/landmarks'
+  end
+
 end
